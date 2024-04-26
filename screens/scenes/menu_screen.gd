c9bcc61a2b8bb362b98d4bacb4865fbd0c62a5d4
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2024-04-26 15:06:58 -0300

    corrigindo problema do som

--- screens/scenes/menu_screen.gd
+++ screens/scenes/menu_screen.gd
@@ -1,20 +1,22 @@
 extends Control
 
+var clickSoundPlayer
+var soundSource = ""
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+	clickSoundPlayer = %clickSound
+	
 func _on_quit_buton_pressed():
-	$clickSound.play()
-	get_tree().quit()
+	soundSource = "quit"
+	clickSoundPlayer.play()
 
 func _on_start_button_pressed():
-	$clickSound.play()
-	get_tree().change_scene_to_file("res://teste.tscn")
+	soundSource = "start"
+	clickSoundPlayer.play()
+
+func _on_click_sound_finished():
+	if soundSource == "start": 
+		get_tree().change_scene_to_file("res://teste.tscn") 
+	elif soundSource == "quit":
+		get_tree().quit()
+	soundSource = ""
