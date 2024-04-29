1e4533d5307beca2d3fa8c147c9dd37f0f887214
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2024-04-29 20:26:34 -0300

    HUD: Timer

--- screens/scripts/hud.gd
+++ screens/scripts/hud.gd
@@ -2,6 +2,10 @@ extends CanvasLayer
 
 @export var equipe: String = "-"
 @onready var camera = $MiniMap/SubViewport/Camera2D
+@onready var timer = $Timer
+
+func _process(delta):
+	$Timer/LabelTimer.text = str(int(timer.time_left / 60)) + ":" + str(int(timer.time_left) % 60)
 
 func _physics_process(delta):
 	camera.position = get_parent().Character.position
@@ -11,8 +15,13 @@ func build_uhd():
 		equipe = "Gatos Hackers"
 	elif get_parent().Character.is_in_group("dogs"):
 		equipe = "Cachorros Vigilantes"	
-	$Control/MarginContainer/VBoxContainer/HBoxContainer/NameTeam.text = equipe
+	$Team/MarginContainer/VBoxContainer/HBoxContainer/NameTeam.text = equipe
+	timer.start()
 	
 
 func _on_button_pressed():
 	get_tree().quit()
+
+
+func _on_timer_timeout():
+	timer.stop()
