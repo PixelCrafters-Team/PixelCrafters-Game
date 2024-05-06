01ee21141018472b3aaa29f46bc61b8ae4f6a92d
Caroline Souza Camargo <carolinecamargo012@gmail.com>
2024-05-06 20:26:02 -0300

    feat: Implementadas função de edição do nome do jogador e botão de mudo

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var menu_screen = preload("res://screens/scenes/menu_screen.tscn")
 var game_scene = preload("res://game/scenes/game.tscn").instantiate()
+var player_name = "Player-" + str(randi() % 100 + 1)
 @onready var click_sound = $ClickSound
 @onready var music_menu = $MusicMenu
 
--- screens/scripts/settings_screen.gd
+++ screens/scripts/settings_screen.gd
@@ -11,12 +11,17 @@ func _ready():
 	value = sound_player.get_volume_db()
 	$MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/HSliderSound.value = value
 	
+	var name_player = get_parent().player_name
+	$MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/TextPlayer.text = name_player
+	
 
 func _on_exit_button_pressed():
+	get_parent().click_sound.play()
 	get_tree().quit()
 
 
 func _on_return_button_pressed():
+	get_parent().click_sound.play()
 	get_parent().add_child(menu_scene.instantiate())
 	get_parent().get_node("SettingsScreen").queue_free()
 
@@ -25,14 +30,66 @@ func _on_h_slider_music_value_changed(value):
 	var music_player = get_parent().get_node("MusicMenu")
 	if music_player:
 		music_player.set_volume_db(float(value))
+		if music_player.get_volume_db() > -80.0:
+			var texture_resource = load("res://screens/themes/style_button_music.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOn.png")
+		else:
+			var texture_resource = load("res://screens/themes/style_button_music.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOff.png")
 	else:
 		print("Nó AudioStreamPlayer não encontrado!")
 
 
 func _on_h_slider_sound_value_changed(value):
+	get_parent().click_sound.play()
 	var sound_player = get_parent().get_node("ClickSound")
 	if sound_player:
 		sound_player.set_volume_db(float(value))
+		if sound_player.get_volume_db() > -80.0:
+			var texture_resource = load("res://screens/themes/style_button_sound.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOn.png")
+		else:
+			var texture_resource = load("res://screens/themes/style_button_sound.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOff.png")
 	else:
 		print("Nó AudioStreamPlayer não encontrado!")
 
+
+func _on_button_music_pressed():
+	get_parent().click_sound.play()
+	var music_player = get_parent().get_node("MusicMenu")
+	if music_player:
+		if music_player.get_volume_db() > -80.0:
+			music_player.set_volume_db(-80.0)
+			var texture_resource = load("res://screens/themes/style_button_music.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOff.png")
+			$MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/HSliderMusic.value = -80.0
+		else:
+			music_player.set_volume_db(0.0)
+			var texture_resource = load("res://screens/themes/style_button_music.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOn.png")
+			$MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/HSliderMusic.value = 0.0
+	else:
+		print("Nó AudioStreamPlayer não encontrado!")
+
+
+func _on_button_sound_pressed():
+	get_parent().click_sound.play()
+	var sound_player = get_parent().get_node("ClickSound")
+	if sound_player:
+		if sound_player.get_volume_db() > -80.0:
+			sound_player.set_volume_db(-80.0)
+			var texture_resource = load("res://screens/themes/style_button_sound.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOff.png")
+			$MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/HSliderSound.value = -80.0
+		else:
+			sound_player.set_volume_db(0.0)
+			var texture_resource = load("res://screens/themes/style_button_sound.tres")
+			texture_resource.texture = load("res://screens/assets/icons/Icon_SoundOn.png")
+			$MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer2/HSliderSound.value = 0.0
+	else:
+		print("Nó AudioStreamPlayer não encontrado!")
+
+
+func _on_text_player_text_changed():
+	get_parent().player_name = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/TextPlayer.text
