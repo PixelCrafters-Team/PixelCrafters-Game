240a77accacfacbec27f8e9008c28e386ef9d44a
Yasmin Souza Camargo <yasmincamargo012@gmail.com>
2024-07-22 18:39:58 -0300

    fix: corrigindo bug na contabilização do numero de gatos congelados

--- characters/scripts/character.gd
+++ characters/scripts/character.gd
@@ -89,7 +89,6 @@ func _physics_process(delta):
 		"""	
 		if Input.is_key_pressed(KEY_Z):
 			if is_in_group("cats"):
-				print("cats")
 				check_and_unfreeze_nearby_cats()
 				
 		if Input.is_key_pressed(KEY_X):
@@ -132,26 +131,28 @@ func _on_timer_timeout():
 
 @rpc
 func glace_cat(player_glace=self.nickname, play_effect=true):
-	get_parent().num_glace_cats += 1
 	var list_players = Networking.return_list()
 	for i in range(list_players.size()):
 		if player_glace == list_players[i][1]:
-			get_parent().get_node(player_glace+'/TextureGlace').visible = true
-			get_parent().get_node(player_glace+'/Texture').visible = false
-			get_parent().get_node(player_glace).is_glace = true
+			if get_parent().get_node(player_glace+'/TextureGlace').visible == false:
+				get_parent().num_glace_cats += 1
+				get_parent().get_node(player_glace+'/TextureGlace').visible = true
+				get_parent().get_node(player_glace+'/Texture').visible = false
+				get_parent().get_node(player_glace).is_glace = true
 			
 			if player_glace == get_name_player() or play_effect == true:
 				get_parent().get_node(player_glace+'/EffectGlace').play()
 
 @rpc
 func unfreeze_cat(player_glace=self.nickname, play_effect=true):
-	get_parent().num_glace_cats -= 1
 	var list_players = Networking.return_list()
 	for i in range(list_players.size()):
 		if player_glace == list_players[i][1]:
-			get_parent().get_node(player_glace+'/TextureGlace').visible = false
-			get_parent().get_node(player_glace+'/Texture').visible = true
-			get_parent().get_node(player_glace).is_glace = false
+			if 	get_parent().get_node(player_glace+'/TextureGlace').visible == true:
+				get_parent().num_glace_cats -= 1
+				get_parent().get_node(player_glace+'/TextureGlace').visible = false
+				get_parent().get_node(player_glace+'/Texture').visible = true
+				get_parent().get_node(player_glace).is_glace = false
 			
 			if player_glace == get_name_player() or play_effect == true:
 				get_parent().get_node(player_glace+'/EffectGlace').play()
@@ -160,11 +161,8 @@ func unfreeze_cat(player_glace=self.nickname, play_effect=true):
 func check_and_unfreeze_nearby_cats():
 	var area = get_node("AreaCollision")
 	var overlapping_bodies = area.get_overlapping_areas()
-	print(overlapping_bodies)
 	for body in overlapping_bodies:			
 		var player_glace = body.get_parent().nickname
-		print(body.get_parent().is_in_group("cats"))
-		print(body.get_parent().get_node('TextureGlace').visible)
 		if body.get_parent().is_in_group("cats") and body.get_parent().get_node('TextureGlace').visible:
 			unfreeze_cat(player_glace)
 			rpc('unfreeze_cat', player_glace, false)
@@ -293,6 +291,7 @@ func _on_area_collision_area_entered(area):
 			rpc('glace_cat', player_glace, false)
 			set_message_game_hud("Jogador " + $namePlayer.text + " congelou jogador " + player_glace, false)
 			rpc("set_message_game_hud", "Jogador " + $namePlayer.text + " congelou jogador " + player_glace)
+		
 		"""	
 		if area.get_parent().is_in_group("cats") and self.is_in_group("cats"):
 			if get_node('TextureGlace').visible == true:
